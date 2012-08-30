c39b12539187e6113ebf9c413b599871c47702cc
Pepe Doval <pepellou@gmail.com>
2012-08-30 14:26:16 +0200

    Refactor

--- src/scan.sh
+++ src/scan.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-while read line
-do
-	user=`echo $line | awk 'BEGIN { FS="|" } { print $1 }'`;
-	pid=`echo $line | awk 'BEGIN { FS="|" } { print $2 }'`;
-	cpu=`echo $line | awk 'BEGIN { FS="|" } { print $3 }'`;
-	mem=`echo $line | awk 'BEGIN { FS="|" } { print $4 }'`;
-	vsz=`echo $line | awk 'BEGIN { FS="|" } { print $5 }'`;
-	rss=`echo $line | awk 'BEGIN { FS="|" } { print $6 }'`;
-	tty=`echo $line | awk 'BEGIN { FS="|" } { print $7 }'`;
-	stat=`echo $line | awk 'BEGIN { FS="|" } { print $8 }'`;
-	start=`echo $line | awk 'BEGIN { FS="|" } { print $9 }'`;
-	time=`echo $line | awk 'BEGIN { FS="|" } { print $10 }'`;
-	cmd=`echo $line | awk 'BEGIN { FS="|" } { print $11 }'`;
+function scan {
+	while read line
+	do
+		user=`extract "$line" 1`
+		start=`extract "$line" 9`
+		cmd=`extract "$line" 11`
+	
+		echo "$start $user $cmd";
+	done < <(getAllProcesses | parse)
+}
 
-	echo "$start $user $cmd";
-done < <(ps aux | tail -n +2 | sort -k 9 | awk '$9 ~ /^[0-9][0-9]:[0-9][0-9]$/ {print $1 "|" $2 "|" $3 "|" $4 "|" $5 "|" $6 "|" $7 "|" $8 "|" $9 "|" $10 "|" substr($0, index($0,$11))}')
+function extract {
+	echo $1 | awk -v i=$2 'BEGIN { FS="|" } { print $i }'
+}
+
+function getAllProcesses {
+	ps aux | tail -n +2 | sort -k 9
+}
+
+function parse {
+	awk '$9 ~ /^[0-9][0-9]:[0-9][0-9]$/ {print $1 "|" $2 "|" $3 "|" $4 "|" $5 "|" $6 "|" $7 "|" $8 "|" $9 "|" $10 "|" substr($0, index($0,$11))}'
+}
+
+scan
